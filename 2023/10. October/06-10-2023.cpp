fb176732517093e084638b2bc47e67bdad9996ac
sankha-mondal <99641675+sankha-mondal@users.noreply.github.com>
2023-10-06 21:53:07 +0530

    06-10-2023 gfg-potd done

--- 2023/10. October/06-10-2023.cpp
+++ 2023/10. October/06-10-2023.cpp
@@ -22,7 +22,8 @@ LinkedList = 10->4->9->1->3->5->9->4
 Output: 
 10 9 3 9 4 5 1 4
 Explanation: 
-Alternative nodes in the given linked list are 4,1,5,4. Reversing the alternative nodes from the given list, and then appending them to the end of the list results in a list 10->9->3->9->4->5->1->4.
+Alternative nodes in the given linked list are 4,1,5,4. Reversing the alternative nodes from the given list, 
+and then appending them to the end of the list results in a list 10->9->3->9->4->5->1->4.
 Example 2:
 
 Input:
@@ -30,9 +31,11 @@ LinkedList = 1->2->3->4->5
 Output: 
 1 3 5 4 2 
 Explanation:
-Alternative nodes in the given linked list are 2 and 4. Reversing the alternative nodes from the given list, and then appending them to the end of the list results in a list 1->3->5->4->2.
+Alternative nodes in the given linked list are 2 and 4. Reversing the alternative nodes from the given list, 
+and then appending them to the end of the list results in a list 1->3->5->4->2.
 Your Task:
-You don't have to read input or print anything. Your task is to complete the function rearrange() which takes the head of the linked list as input and rearranges the list as required.
+You don't have to read input or print anything. Your task is to complete the function rearrange() which takes 
+the head of the linked list as input and rearranges the list as required.
 
 Expected Time Complexity: O(N)
 Expected Auxiliary Space: O(1)
